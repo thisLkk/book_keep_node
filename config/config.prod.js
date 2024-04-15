5db117e66e4e94bd46e4aa798d753b1be54cf98f
lukuankuan <289137225@qq.com>
2024-04-15 20:41:07 +0800

    feat: 增加cors中间件

--- config/config.prod.js
+++ config/config.prod.js
@@ -17,6 +17,12 @@ module.exports = appInfo => {
     password: 'hnunu123',
     database: 'hnunu',
   }
+  config.cors = {  
+    origin: '*', // 允许来自任何源的访问  
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH', // 允许的方法  
+    credentials: true, // 是否发送cookie  
+    allowHeaders: 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With', // 允许的HTTP头  
+  }
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1712759179323_52751';
 
--- config/plugin.js
+++ config/plugin.js
@@ -7,5 +7,9 @@ module.exports = {
   sequelize: {
     enable: true,
     package: 'egg-sequelize'
+  },
+  cors: {  
+    enable: true,  
+    package: 'egg-cors',  
   }
 };
